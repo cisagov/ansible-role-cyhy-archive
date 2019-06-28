73bcca2137031f194d85797462e50e821d85b2b1
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-06-28 15:45:24 -0400

    Fix typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "iam_user" {
-  source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-moduleXS"
+  source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-module"
 
   ssm_parameters = ["/github/oauth_token"]
   user_name      = "test-ansible-role-cyhy-archive"
